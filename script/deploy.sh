df4a4aacea8ea570adb004a1736fc5fe84c11809
swat <12694496+swat9013@users.noreply.github.com>
2024-10-08 12:27:06 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,8 @@
 
 echo "start deploy"
 
+set -x
+
 sudo systemctl stop isupipe-ruby.service
 
 sudo rm /var/log/nginx/access.log
@@ -10,5 +12,7 @@ sudo nginx -s reload
 sudo systemctl restart mysql
 
 sudo systemctl start isupipe-ruby.service
+
+set +x
 
 echo "finish deploy"
