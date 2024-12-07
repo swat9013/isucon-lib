28356c78637be4d740cb14b15e104d0c9e0c18e0
swat <12694496+swat9013@users.noreply.github.com>
2024-12-07 09:13:03 +0900

    Update clean_log.sh

--- script/clean_log.sh
+++ script/clean_log.sh
@@ -5,5 +5,6 @@ cd "$(dirname "$0")"/../ || exit
 set -x
 sudo sh -c "echo > /var/log/mysql/mysql-slow.log"
 sudo sh -c "echo > /var/log/nginx/access.log"
+sh -c "echo > ~/webapp/ruby/production.log"
 set +x
 
